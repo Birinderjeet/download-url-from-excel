ad4137c5149f5a1895a4889af58e4a7a4ade9a6a
Birinderjeet <45066810+Birinderjeet@users.noreply.github.com>
2019-08-28 15:35:47 +0530

    commenting

--- img_pro.py
+++ img_pro.py
@@ -1,11 +1,17 @@
+#import lib
 import requests
 import pandas as pd
+
+#get excel file from storage
 file_errors_location = 'C:/Users/Birinderjeet/Desktop/image downlaoder/excel file/url.xlsx'
 df = pd.read_excel(file_errors_location)
 
 for i in range(0,len(df['url'])):
     print(i)
+    #set name of images
     p = 'images/'+str(df['name'][i])+'.jpg'
+
+    #downloading file from url
     r = requests.get(df['url'][i], stream=True)
     with open(p, 'wb') as f:
         for chunk in r:
